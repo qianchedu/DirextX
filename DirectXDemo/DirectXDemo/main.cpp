615171a241ebd1b521a7a65cf8094a4cfed9dcb5
qianchedu <13713659927@163.com>
2019-09-05 22:11:50 +0800

    04   第1章 创建和显示Direct3D窗口 (第13页)

--- DirectXDemo/DirectXDemo/main.cpp
+++ DirectXDemo/DirectXDemo/main.cpp
@@ -1,7 +1,20 @@
 #include  <Windows.h>
+#include <d3d9.h>
+
+#pragma comment(lib,"d3d9.lib");
+#pragma comment(lib,"d3dx9.lib");
 
 #define WINDOW_CLASS "UGPDX"
 #define WINDOW_TITLE "Deme window"
+
+
+
+LPDIRECT3D9 g_D3D = NULL;
+LPDIRECT3DDEVICE9 g_D3DDevice = NULL;
+
+bool InitializeD3D(HWND hWnd);
+void RenderScene();
+void Shutdown();
 
 LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -44,7 +57,7 @@ int WINAPI WinMain(HINSTANCE hInst,
 		hInst,
 		NULL,
 		NULL,
-		(HBRUSH)GetStockObject(WHITE_BRUSH),
+		NULL,
 		NULL,
 		WINDOW_CLASS,
 		NULL
@@ -57,28 +70,84 @@ int WINAPI WinMain(HINSTANCE hInst,
 	HWND hWnd = CreateWindow(WINDOW_CLASS, WINDOW_TITLE,WS_OVERLAPPEDWINDOW,
 		100,100,640,480,GetDesktopWindow(),NULL,hInst,NULL);
 
-	ShowWindow(hWnd, SW_SHOWDEFAULT);
 
-	UpdateWindow(hWnd);
-
-
-	MSG msg;
-	ZeroMemory(&msg,sizeof(msg));
-
-	while (msg.message != WM_QUIT)
+	if (InitializeD3D(hWnd))
 	{
 
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		ShowWindow(hWnd, SW_SHOWDEFAULT);
+
+		UpdateWindow(hWnd);
+
+
+		MSG msg;
+		ZeroMemory(&msg,sizeof(msg));
+
+		while (msg.message != WM_QUIT)
 		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-		else 
-		{
-			//没有消息要处理，就可以做其他的一些工作
-		}
+
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else 
+			{
+				//没有消息要处理，就可以做其他的一些工作
+				//RenderScene();		//渲染场景
+			}
 	
+		}
 	}
 
+	Shutdown();
 	UnregisterClass(WINDOW_CLASS, hInst);
+}
+
+
+bool InitializeD3D(HWND hWnd) 
+{
+	D3DDISPLAYMODE displayMode;
+	g_D3D = Direct3DCreate9(D3D_SDK_VERSION);
+
+	if (g_D3D == NULL) return false;
+
+	if (FAILED(g_D3D ->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &displayMode)))
+	{
+		return false;
+	}
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp,sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.BackBufferFormat = displayMode.Format;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+
+	if (FAILED(g_D3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &g_D3DDevice)))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+void Shutdown()
+{
+	if (g_D3DDevice != NULL)
+		g_D3DDevice->Release();
+	if (g_D3D != NULL)
+		g_D3D->Release();
+}
+
+
+void RenderScene()
+{
+	g_D3DDevice->Clear(0,NULL,D3DCLEAR_TARGET,D3DCOLOR_XRGB(0,0,0),1.0f,0);
+	g_D3DDevice->BeginScene();
+
+	//输出3d图形
+
+	g_D3DDevice->EndScene();
+
+	g_D3DDevice->Present(NULL,NULL,NULL,NULL);
 }
