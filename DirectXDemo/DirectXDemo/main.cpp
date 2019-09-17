29a0d7251c48bfbda50d241fc766db3ea873ee11
qianchedu <13713659927@163.com>
2019-09-17 10:14:29 +0800

    09   第1章 模板源文件 (第43页) 模板

--- DirectXDemo/DirectXDemo/main.cpp
+++ DirectXDemo/DirectXDemo/main.cpp
@@ -2,8 +2,8 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 
-#pragma comment(lib,"d3d9.lib");
-#pragma comment(lib,"d3dx9.lib");
+#pragma comment(lib,"d3d9.lib")
+#pragma comment(lib,"d3dx9.lib")
 
 #define WINDOW_CLASS    "UGPDX"
 #define WINDOW_TITLE    "Deme window"
@@ -11,7 +11,10 @@
 #define WINDOW_HEIGHT   480
 
 
-
+bool InitializeD3D(HWND hWnd,bool fullscreen);
+bool InitializeObjects();
+void RenderScene();
+void Shutdown();
 
 LPDIRECT3D9 g_D3D = NULL;
 LPDIRECT3DDEVICE9 g_D3DDevice = NULL;
@@ -21,26 +24,13 @@ LPDIRECT3DDEVICE9 g_D3DDevice = NULL;
 
 D3DXMATRIX g_projection;
 D3DXMATRIX g_ViewMatrix;
-//D3DXMATRIX g_translation;
-//D3DXMATRIX g_rotation;
+D3DXMATRIX g_WorldMatrix;
+
 
 float g_angle = 0.0f;
 
 LPDIRECT3DVERTEXBUFFER9 g_VertexBuffer = NULL;
 
-
-bool InitializeD3D(HWND hWnd);
-bool InitializeObjects();
-void RenderScene();
-void Shutdown();
-
-struct stD3DVertex 
-{
-	//float x, y, z, rhw;
-	float x, y, z;
-	unsigned long color;
-
-};
 
 #define D3DFVF_VERTEX (D3DFVF_XYZ|D3DFVF_DIFFUSE)
 
@@ -100,7 +90,7 @@ int WINAPI WinMain(HINSTANCE hInst,
 		100,100,WINDOW_WIDTH,WINDOW_HEIGHT,GetDesktopWindow(),NULL,hInst,NULL);
 
 
-	if (InitializeD3D(hWnd))
+	if (InitializeD3D(hWnd,false))
 	{
 
 		ShowWindow(hWnd, SW_SHOWDEFAULT);
@@ -133,7 +123,7 @@ int WINAPI WinMain(HINSTANCE hInst,
 }
 
 
-bool InitializeD3D(HWND hWnd) 
+bool InitializeD3D(HWND hWnd,bool fullscreen) 
 {
 	D3DDISPLAYMODE displayMode;
 	g_D3D = Direct3DCreate9(D3D_SDK_VERSION);
@@ -147,7 +137,20 @@ bool InitializeD3D(HWND hWnd)
 
 	D3DPRESENT_PARAMETERS d3dpp;
 	ZeroMemory(&d3dpp,sizeof(d3dpp));
-	d3dpp.Windowed = TRUE;
+
+	if (fullscreen)
+	{
+		d3dpp.Windowed = FALSE;
+		
+		d3dpp.BackBufferWidth = WINDOW_WIDTH;
+		d3dpp.BackBufferHeight = WINDOW_HEIGHT;	
+	}
+	else 
+	{
+		d3dpp.Windowed = TRUE;
+	}
+
+	
 	d3dpp.BackBufferFormat = displayMode.Format;
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
 
@@ -165,11 +168,9 @@ bool InitializeD3D(HWND hWnd)
 
 void Shutdown()
 {
-	if (g_D3DDevice != NULL)
-		g_D3DDevice->Release();
-	if (g_D3D != NULL)
-		g_D3D->Release();
-	if (g_VertexBuffer)g_VertexBuffer->Release();
+	if (g_D3DDevice != NULL) g_D3DDevice->Release();
+	if (g_D3D != NULL) g_D3D->Release();
+	if (g_VertexBuffer) g_VertexBuffer->Release();
 
 	g_D3DDevice = NULL;
 	g_D3D = NULL;
@@ -183,26 +184,6 @@ void RenderScene()
 	g_D3DDevice->BeginScene();
 
 	g_D3DDevice->SetTransform(D3DTS_VIEW, &g_ViewMatrix);
-
-	////创建一个矩阵
-	//D3DXMatrixTranslation(&g_translation,0.0f,0.0f,3.0f);
-	////沿着中心轴的Y轴旋转
-	//D3DXMatrixRotationY(&g_rotation, g_angle);
-	//g_D3DDevice->SetTransform(D3DTS_WORLD, &g_worldMatrix);
-
-	//g_worldMatrix = g_rotation * g_translation;
-
-	//g_angle += 0.01f;
-
-	//if (g_angle >= 360) g_angle = 0.0f;
-
-
-	//输出3d图形
-	g_D3DDevice->SetStreamSource(0, g_VertexBuffer,0,sizeof(stD3DVertex));
-
-	g_D3DDevice->SetFVF(D3DFVF_VERTEX);
-
-	g_D3DDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
 
 	
 
@@ -221,45 +202,14 @@ bool InitializeObjects()
 	D3DXMatrixPerspectiveFovLH(&g_projection,45.0f,WINDOW_WIDTH/WINDOW_HEIGHT,0.1f,1000);
 
 	g_D3DDevice->SetTransform(D3DTS_PROJECTION, &g_projection);
+	g_D3DDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
 
 	D3DXVECTOR3 cameraPos(0.0f, 0.0f, -5.0f);
 	D3DXVECTOR3 lookAtPos(0.0f, 0.0f,0.0f);
 	D3DXVECTOR3 upDir(0.0f,1.0f,0.0f);
 
 	D3DXMatrixLookAtLH(&g_ViewMatrix, &cameraPos, &lookAtPos, &upDir);
-
-
-
-	g_D3DDevice->SetRenderState(D3DRS_LIGHTING,FALSE);
-	g_D3DDevice->SetRenderState(D3DRS_CULLMODE,D3DCULL_NONE);
-
-	unsigned long col = D3DCOLOR_XRGB(255, 255, 255);
-
 	
-
-	stD3DVertex objData[] =
-	{
-		{ -0.3f,-0.3f,0.0f,D3DCOLOR_XRGB(255,255,0)},
-		{  0.3f,-0.3f,0.0f,D3DCOLOR_XRGB(255,0,  0)},
-		{  0.0f, 0.3f,0.0f,D3DCOLOR_XRGB(0,  0,255)},
-	};
-
-
-
-
-	if (FAILED(g_D3DDevice->CreateVertexBuffer(sizeof(objData), 0, D3DFVF_VERTEX, D3DPOOL_DEFAULT, &g_VertexBuffer, NULL)))
-	{
-		return false;
-	}
-
-
-	void *ptr;
-
-	if (FAILED(g_VertexBuffer->Lock(0, sizeof(objData), (void**)&ptr, 0)))
-		return false;
-
-	memcpy(ptr, objData,sizeof(objData));
-
-	g_VertexBuffer->Unlock();
+	
 	return true;
 }
